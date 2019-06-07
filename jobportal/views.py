8b558f25dc94aa6b8a3105ed905e036b9923be30
Andrew Zulaybar <andrewzulaybar@gmail.com>
2019-06-07 13:54:16 -0700

    Added home page view.

--- jobportal/views.py
+++ jobportal/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    context = {
+        'title': 'Home Page'
+    }
+    return render(request, 'jobportal/base.html', context)
