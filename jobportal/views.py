914be567d20c5a0977efdb0b2f0d9c75b65f18c1
chntlc <chantaalchung@gmail.com>
2019-06-07 23:35:30 -0700

    Added card.

--- jobportal/views.py
+++ jobportal/views.py
@@ -3,6 +3,23 @@ from django.shortcuts import render
 
 def home(request):
     context = {
-        'title': 'Home Page'
+        'title': 'Home Page',
+        'jobs': [{
+            'job1': {
+                'title': 'Software Developer Co-Op',
+                'company': 'Amazon',
+                'deadline': '5',
+                'applications': '65',
+                'salary': '30,000',
+                'sector': 'Tech',
+                'city': 'Seattle',
+                'prov_state': 'WA'
+            },
+            'job2': {
+                'title': 'Data Analyst',
+                'company': 'SAP'
+            }
+        }],
+        'job_list': ['Amazon', 'Apple', 'Google']
     }
     return render(request, 'jobportal/base.html', context)
