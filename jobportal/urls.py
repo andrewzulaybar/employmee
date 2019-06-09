bb047996087d29c9bbcdc3d85e38d9b7c77fcc10
chntlc <chantaalchung@gmail.com>
2019-06-08 20:32:41 -0700

    Added url paths.

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -4,4 +4,7 @@ from jobportal import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('premium', views.premium_home, name='premium-home'),
+    path('company', views.company_home, name='company-home'),
+    path('details/1', views.details, name='details'),
 ]
--- jobportal/views.py
+++ jobportal/views.py
@@ -1,29 +1,41 @@
 from django.shortcuts import render
 
+CONTEXT = {
+    'title': 'Home Page',
+    'jobs': [{
+        'job1': {
+            'id': '1',
+            'title': 'Software Developer Co-Op',
+            'company': 'Amazon',
+            'deadline': '5',
+            'applications': '65',
+            'salary': '30,000',
+            'sector': 'Tech',
+            'city': 'Seattle',
+            'prov_state': 'WA',
+            'type': 'Internship',
+            'education': 'High school',
+            'skills': ['java', 'c++'],
+        },
+        'job2': {
+            'title': 'Data Analyst',
+            'company': 'SAP'
+        }
+    }]
+}
+
 
 def home(request):
-    context = {
-        'title': 'Home Page',
-        'jobs': [{
-            'job1': {
-                'id': '1',
-                'title': 'Software Developer Co-Op',
-                'company': 'Amazon',
-                'deadline': '5',
-                'applications': '65',
-                'salary': '30,000',
-                'sector': 'Tech',
-                'city': 'Seattle',
-                'prov_state': 'WA',
-                'type': 'Internship',
-                'education': 'High school',
-                'skills': ['java', 'c++'],
-            },
-            'job2': {
-                'title': 'Data Analyst',
-                'company': 'SAP'
-            }
-        }],
-        'job_list': ['Amazon', 'Apple', 'Google']
-    }
-    return render(request, 'jobportal/details.html', context)
+    return render(request, 'jobportal/home.html', CONTEXT)
+
+
+def premium_home(request):
+    return render(request, 'jobportal/home-prem.html', CONTEXT)
+
+
+def company_home(request):
+    return render(request, 'jobportal/home-comp.html', CONTEXT)
+
+
+def details(request):
+    return render(request, 'jobportal/details.html', CONTEXT)
