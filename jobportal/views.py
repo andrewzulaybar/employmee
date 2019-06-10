9e1ce700e365e4add36422c38223da699a74b540
Andrew Zulaybar <andrewzulaybar@gmail.com>
2019-06-09 18:19:24 -0700

    Refactored home page and sidebar.

--- jobportal/views.py
+++ jobportal/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 CONTEXT = {
     'title': 'Home Page',
@@ -16,12 +17,22 @@ CONTEXT = {
             'type': 'Internship',
             'education': 'High school',
             'skills': ['java', 'c++'],
+            'description': """We are looking for Amazon interns to join us for Fall 2019! Amazon interns have the 
+                            opportunity to work alongside the industry’s brightest engineers who innovate everyday on 
+                            behalf of our customers. Our interns and co-ops write real software and partner with a 
+                            select group of experienced software development engineers, who both help and challenge 
+                            them as they work on projects that matter..."""
         },
         'job2': {
             'title': 'Data Analyst',
             'company': 'SAP'
         }
-    }]
+    }],
+    'sectors': ['Technology', 'Medical', 'Marketing'],
+    'skills': ['Java', 'C++', 'Python'],
+    'cities': ['Vancouver, BC', 'Portland, OR', 'Toronto, ON'],
+    'education': ['No Minimum', 'High School', 'Bachelors', 'Masters', 'PhD'],
+    'types': ['Part-Time', 'Full-Time', 'Contract', 'Internship']
 }
 
 
