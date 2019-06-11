1a0218a4c700168a55a79a612eebb9e5d0cd09b4
regina <rlarcilla@hotmail.com>
2019-06-11 15:57:51 -0700

    added city and skill into query

--- jobportal/dbqueries.py
+++ jobportal/dbqueries.py
@@ -8,20 +8,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'cpsc304.settings')
 
 
-# job -> sector, deadline, education, type,
-# requires -> skill todo
-# job_location -> zip todo
-# Location -> city, postalzip (join with job_location) todo
 class JobQuery:
     def __init__(self, sector_list, edu_list, type_list, skill_list, city_list, deadline, recent_post, sort_by):
         self.sector_list = ["sector = '{}'".format(x) for x in sector_list]
         self.edu_list = ["min_education = '{}'".format(x) for x in edu_list]
         self.type_list = ["employment_type = '{}'".format(x) for x in type_list]
 
-        self.skill_list = skill_list
-        self.city_list = city_list
+        self.skill_list = ["skill_name = '{}'".format(x) for x in skill_list]
+        self.city_list = ["city = '{}'".format(x) for x in city_list]
 
-        #todo: confirm
         self.deadline = "deadline <= {}".format(date.today() + timedelta(days=deadline)) if deadline else ""
         self.recent_post = "date >= {}".format(date.today() - timedelta(days=recent_post)) if recent_post else ""
 
@@ -29,11 +24,19 @@ class JobQuery:
 
     def mysql_query(self):
         with connection.cursor() as cursor:
+            from_str = "Job"
+
+            if self.skill_list and self.city_list:
+                from_str += " INNER JOIN Job_Location ON Job.job_ID = Job_Location.job_ID INNER JOIN Location ON Job_Location.postal_zip = Location.postal_zip INNER JOIN Requires ON Job.job_ID = Requires.job_ID"
+            elif self.skill_list:
+                from_str += " INNER JOIN Requires ON Job.job_ID = Requires.job_ID"
+            elif self.city_list:
+                from_str += " INNER JOIN Job_Location ON Job.job_ID = Job_Location.job_ID INNER JOIN Location ON Job_Location.postal_zip = Location.postal_zip"
 
             if not self.sort_by:
-                sql = "SELECT * FROM Job %s;" % self.build_where_content()
+                sql = "SELECT * FROM %s %s;" % (from_str, self.build_where_content())
             else:
-                sql = "SELECT * FROM Job %s ORDER BY %s;" % (self.build_where_content(), self.sort_by)
+                sql = "SELECT * FROM %s %s ORDER BY %s;" % (from_str, self.build_where_content(), self.sort_by)
 
             sql.replace("'", "\\'")
             cursor.execute(sql)
@@ -43,7 +46,8 @@ class JobQuery:
 
     def build_where_content(self):
         non_empty_attrs = filter(lambda x: bool(x),
-                                 [self.sector_list, self.edu_list, self.type_list, self.deadline, self.recent_post])
+                                 [self.sector_list, self.edu_list, self.type_list, self.skill_list,
+                                  self.city_list, self.deadline, self.recent_post])
         processed_lst = []
 
         for attr in non_empty_attrs:
@@ -62,16 +66,29 @@ class JobQuery:
 
 
 if __name__ == '__main__':
-    myQuery = JobQuery(["Technology", "Medical"], [], [], [], [], '', '', 'deadline')
-    example = myQuery.mysql_query()
+    # sector_list, edu_list, type_list, skill_list, city_list, deadline, recent_post, sort_by
+
+    myQuery1 = JobQuery(["Technology", "Medical"], [], [], [], [], '', '', 'deadline')
+    example1 = myQuery1.mysql_query()
 
     myQuery2 = JobQuery([], ['Bachelors', 'Masters'], [], [], [], '', '', 'company_login_id')
     example2 = myQuery2.mysql_query()
 
-    print('This is Query1')
-    print(example)
+    myQuery3 = JobQuery([], ['Bachelors', 'Masters'], [], [], ['Vancouver', 'New York'], '', '', 'company_login_id')
+    example3 = myQuery3.mysql_query()
+
+    myQuery4 = JobQuery([], ['Bachelors', 'Masters'], [], ['C++'], ['New York'], '', '', '')
+    example4 = myQuery4.mysql_query()
+
+    print(example1)
     print()
     print()
-    print('This is Query2')
     print(example2)
+    print()
+    print()
+    print(example3)
+    print()
+    print()
+    print(example4)
+
 
