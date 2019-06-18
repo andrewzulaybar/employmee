2e730157d98fa7b349a070150b1280ca1d864d7e
emecke <mle_m@mac.com>
2019-06-17 17:26:43 -0700

    added try catch to insertion adn deletion from saved jobs to stop errors when a duplicate insert or delete is attempted

--- jobportal/savejob.py
+++ jobportal/savejob.py
@@ -10,15 +10,19 @@ def save_prem_job(username, job_id):
     with connection.cursor() as cursor:
         query = """INSERT INTO saves_job(premium_login_ID, job_ID) VALUES ('%s', %s)""" % (username, job_id)
         print(query)
-        result = cursor.execute(query)
-        print(result)
-        print ("Record inserted successfully into saves_job table")
+        try:
+            cursor.execute(query)
+            print ("Record inserted successfully into saves_job table")
+        except:
+            print('problem inserting %s, %s from saves_job' % (username, job_id))
+
 
 
 def un_save_prem_job(username, job_id):
     with connection.cursor() as cursor:
         query = """DELETE FROM saves_job WHERE premium_login_id='%s' AND job_id='%s'""" % (username, job_id)
-        print(query)
-        result = cursor.execute(query)
-        print(result)
-        print ("Record deleted successfully from saves_job table")
+        try:
+            cursor.execute(query)
+            print ("Record deleted successfully from saves_job table")
+        except:
+            print('problem deleting %s, %s from saves_job' % (username, job_id))
