dee665cece1f0c9424d5ad098d324718ed4016c2
chntlc <chantaalchung@gmail.com>
2019-06-18 14:22:04 -0700

    Changed design for company settings.

--- jobportal/branch.py
+++ jobportal/branch.py
@@ -11,16 +11,28 @@ class Branch:
     def branch_info(self, username, form):
         var = ''
         if len(form.cleaned_data['data']) >= 2:
-            var = 'b.street_address, b.postal_zip, b.contact_email'
+            var = ' , b.street_address, b.postal_zip, b.contact_email'
+        elif len(form.cleaned_data['data']) == 0:
+            var = ''
         elif form.cleaned_data['data'][0] == 'Address':
-            var = 'b.street_address, b.postal_zip'
+            var = ' , b.street_address, b.postal_zip'
         elif form.cleaned_data['data'][0] == 'Contact Email':
-            var = 'b.contact_email'
+            var = ' , b.contact_email'
         with connection.cursor() as cursor:
-            query = "SELECT b.branch_ID, {0} \
+            query = "SELECT b.branch_ID{0} \
                     from Branch b \
                     WHERE b.company_login_id ='{1}' AND b.branch_ID = '{2}';" .format(var, username, form.cleaned_data['id'])
             cursor.execute(query)
             info = cursor.fetchall()
             ids = [branch for branch in info]
+            return ids
+
+    def branch_id(self, username):
+        with connection.cursor() as cursor:
+            query = "SELECT b.branch_ID \
+                    from Branch b \
+                    WHERE b.company_login_id ='{0}';" .format(username)
+            cursor.execute(query)
+            info = cursor.fetchall()
+            ids = [branch[0] for branch in info]
             return ids
