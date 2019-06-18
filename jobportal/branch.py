6e162a6ccf6ad06e4f555fdd49b8cbb502b0aa8d
chntlc <chantaalchung@gmail.com>
2019-06-18 10:59:48 -0700

    Fixed index error for branches.

--- jobportal/branch.py
+++ jobportal/branch.py
@@ -11,13 +11,15 @@ class Branch:
     def branch_info(self, username, form):
         var = ''
         if len(form.cleaned_data['data']) >= 2:
-            var = 'b.street_address, b.postal_zip, b.contact_email'
+            var = ', b.street_address, b.postal_zip, b.contact_email'
+        elif len(form.cleaned_data['data']) == 0:
+            var = ''
         elif form.cleaned_data['data'][0] == 'Address':
-            var = 'b.street_address, b.postal_zip'
+            var = ', b.street_address, b.postal_zip'
         elif form.cleaned_data['data'][0] == 'Contact Email':
-            var = 'b.contact_email'
+            var = ', b.contact_email'
         with connection.cursor() as cursor:
-            query = "SELECT b.branch_ID, {0} \
+            query = "SELECT b.branch_ID{0} \
                     from Branch b \
                     WHERE b.company_login_id ='{1}' AND b.branch_ID = '{2}';" .format(var, username, form.cleaned_data['id'])
             cursor.execute(query)
